d7cf9bbf3c684054a90d5f49565e15ecabd0ef3a
Peter M.J. Herman <33602427+pmjherman@users.noreply.github.com>
2021-09-27 17:22:01 +0200

    extended plot output to all variables in map file

--- tools/plot_output.py
+++ tools/plot_output.py
@@ -18,21 +18,27 @@ url=os.path.join(out_dir,'CoralModel_map.nc')
 nc = netCDF4.Dataset(url)
 nc.variables.keys()
 
-PT = nc.variables['PT'][:].data
+teller = 0
+for vv in nc.variables.keys():
+    teller = teller+1
+    if(teller >3):
+       
+        VT = nc.variables[vv]
+        VarT = VT[:]
+
+        fig = plt.figure()
+        ax = plt.axes()
+        plt.xlim(0, 100)
+        plt.ylim(0, 1.05*np.max(VarT))
+        plt.title(VT.long_name)
+        plt.xlabel("Time (years)")
+        plt.ylabel(VT.units)
+        
+        x = np.linspace(1,100,100)
+        
+        ax.plot(x, VarT[:,1],'-g',label='Cell 1')
+        ax.plot(x, VarT[:,100],'-r',label="Cell 100")
+        ax.plot(x,VarT[:,300],'-c',label='Cell 300')
+        plt.legend();
+
 nc.close()
-
-
-fig = plt.figure()
-ax = plt.axes()
-plt.xlim(0, 100)
-plt.ylim(0, 1.05)
-plt.title("Total Coral Population")
-plt.xlabel("Time (years)")
-plt.ylabel("Relative cover")
-
-x = np.linspace(1,100,100)
-
-ax.plot(x, PT[:,1],'-g',label='Cell 1')
-ax.plot(x, PT[:,100],'-r',label="Cell 100")
-ax.plot(x,PT[:,300],'-c',label='Cell 300')
-plt.legend();
