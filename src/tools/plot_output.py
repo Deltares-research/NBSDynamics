05561c65fc830aedad5aa7862ad428445994eca2
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-10-29 14:56:12 +0200

    refactor(hydrodynamics.py): Refactor the update method to use update_… (#30)
    * refactor(hydrodynamics.py): Refactor the update method to use update_orbital and avoid code duplication
    
    Part of the refactoring based on sonar cloud analysis.
    
    * refactor(plot_output.py): Refactored plotting methods to avoid code duplication
    
    * autoformat: isort & black
    
    Co-authored-by: Carsopre <Carsopre@users.noreply.github.com>

--- src/tools/plot_output.py
+++ src/tools/plot_output.py
@@ -6,12 +6,54 @@ Created on Fri Sep 24 11:36:48 2021
 """
 
 import os
+from typing import Callable
 
 import matplotlib.pyplot as plt
 import netCDF4
 import numpy as np
 
 plt.style.use("seaborn-whitegrid")
+
+
+def subplot_mapfile(var_t, ylims, plot_axes):
+    x = np.linspace(1, 100, 100)
+    plt.ylim(ylims)
+    plot_axes.plot(x, var_t[:, 1], "-g", label="Cell 1")
+    plot_axes.plot(x, var_t[:, 100], "-r", label="Cell 100")
+    plot_axes.plot(x, var_t[:, 300], "-c", label="Cell 300")
+    plt.legend()
+
+
+def subplot_hisfile(var_t, ylims, plot_axes):
+    x = np.linspace(0, 100, 36525)
+    colors = iter(plt.cm.rainbow(np.linspace(0, 1, var_t.shape[1])))
+    for i in range(var_t.shape[1]):
+        plot_axes.plot(x, var_t[:, i], color=next(colors), label=f"Point {i}")
+    plt.legend()
+
+
+def plot_nc_variables(nc_variables, subplot_call: Callable):
+    teller = 0
+    for vv in nc_variables.keys():
+        teller = teller + 1
+        if teller > 3:
+
+            VT = nc_variables[vv]
+            VarT = VT[:]
+
+            fig = plt.figure()
+            ax = plt.axes()
+            plt.xlim(0, 100)
+            ylims = limdict[vv]
+            if ylims[0] == 9999:
+                ylims[0] = 0.95 * np.min(VarT)
+            if ylims[1] == 9999:
+                ylims[1] = 1.05 * np.max(VarT)
+            plt.title(VT.long_name)
+            plt.xlabel("Time (years)")
+            plt.ylabel(VT.units)
+            subplot_call(VarT, VT, ax)
+
 
 # NetCDF4-Python can read a remote OPeNDAP dataset or a local NetCDF file:
 out_dir = os.path.join(
@@ -51,33 +93,7 @@ limdict = {
     "G": [9999, 9999],
 }
 
-teller = 0
-for vv in nc.variables.keys():
-    teller = teller + 1
-    if teller > 3:
-
-        VT = nc.variables[vv]
-        VarT = VT[:]
-
-        fig = plt.figure()
-        ax = plt.axes()
-        plt.xlim(0, 100)
-        ylims = limdict[vv]
-        if ylims[0] == 9999:
-            ylims[0] = 0.95 * np.min(VarT)
-        if ylims[1] == 9999:
-            ylims[1] = 1.05 * np.max(VarT)
-        plt.ylim(ylims)
-        plt.title(VT.long_name)
-        plt.xlabel("Time (years)")
-        plt.ylabel(VT.units)
-
-        x = np.linspace(1, 100, 100)
-
-        ax.plot(x, VarT[:, 1], "-g", label="Cell 1")
-        ax.plot(x, VarT[:, 100], "-r", label="Cell 100")
-        ax.plot(x, VarT[:, 300], "-c", label="Cell 300")
-        plt.legend()
+plot_nc_variables(nc.variables, subplot_mapfile)
 
 nc.close()
 
@@ -85,31 +101,6 @@ nc.close()
 url = os.path.join(out_dir, "CoralModel_his.nc")
 nc = netCDF4.Dataset(url)
 nc.variables.keys()
-
-teller = 0
-for vv in nc.variables.keys():
-    teller = teller + 1
-    if teller > 3:
-
-        VT = nc.variables[vv]
-        VarT = VT[:]
-
-        fig = plt.figure()
-        ax = plt.axes()
-        plt.xlim(0, 100)
-        ylims = limdict[vv]
-        if ylims[0] == 9999:
-            ylims[0] = 0.95 * np.min(VarT)
-        if ylims[1] == 9999:
-            ylims[1] = 1.05 * np.max(VarT)
-        plt.title(VT.long_name)
-        plt.xlabel("Time (years)")
-        plt.ylabel(VT.units)
-
-        x = np.linspace(0, 100, 36525)
-        colors = iter(plt.cm.rainbow(np.linspace(0, 1, VarT.shape[1])))
-        for i in range(VarT.shape[1]):
-            ax.plot(x, VarT[:, i], color=next(colors), label=f"Point {i}")
-        plt.legend()
+plot_nc_variables(nc.variables, subplot_hisfile)
 
 nc.close()
