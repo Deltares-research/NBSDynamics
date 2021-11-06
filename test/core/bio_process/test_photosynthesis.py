65f2e9c8dc2ccd4c09fa8380afdd7b47d010fc07
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-11-06 14:58:20 +0100

    test(test/core/bio_process/test_photosynthesis.py): Added tests for photosynthesis class.

---
+++ test/core/bio_process/test_photosynthesis.py
@@ -0,0 +1,52 @@
+from src.core.bio_process.photosynthesis import Photosynthesis
+import pytest
+import numpy as np
+
+
+class TestPhotosynthesis:
+    def test_ini_photoshynthesis(self):
+        input_dict = dict(constants=None, light_in=None, first_year=None)
+        test_photo = Photosynthesis(**input_dict)
+        assert test_photo.pld == 1
+        assert test_photo.ptd == 1
+        assert test_photo.pfd == 1
+        assert test_photo.constants == None
+
+    @pytest.fixture(autouse=True)
+    def valid_photosynthesis(self) -> Photosynthesis:
+        class TestConstants:
+            pfd = 1
+            pfd_min = 0
+            ucr = 2
+
+        input_dict = dict(constants=TestConstants(), light_in=None, first_year=None)
+        return Photosynthesis(**input_dict)
+
+    @pytest.fixture(autouse=True)
+    def valid_coral(self):
+        class TestCoral:
+            ucm = 1
+
+        return TestCoral()
+
+    def test_flow_dependency_no_pfd(self, valid_photosynthesis: Photosynthesis):
+        valid_photosynthesis.constants.pfd = None
+        valid_photosynthesis.flow_dependency(None)
+        assert valid_photosynthesis.pfd == 1
+
+    def test_flow_dependency_with_pfd(
+        self, valid_photosynthesis: Photosynthesis, valid_coral
+    ):
+        assert valid_photosynthesis.pfd == 1
+        valid_photosynthesis.flow_dependency(valid_coral)
+        assert valid_photosynthesis.pfd[0][0] == pytest.approx(0.76159416)
+
+    def light_dependency_raises_notimplemented_error(
+        self, valid_photosynthesis: Photosynthesis, valid_coral
+    ):
+        with pytest.raises(NotImplementedError) as e_info:
+            valid_photosynthesis.light_dependency(valid_coral, None)
+        assert (
+            str(e_info.value)
+            == "Only the quasi-steady state solution is currently implemented; use key-word 'qss'."
+        )
