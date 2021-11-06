d53cca2d8f446b32e3a7be92d6c622e6d140399b
Carsopre <Carsopre@users.noreply.github.com>
2021-11-06 08:07:40 +0000

    autoformat: isort & black

--- test/core/hydrodynamics/test_hydrodynamics.py
+++ test/core/hydrodynamics/test_hydrodynamics.py
@@ -2,14 +2,13 @@ from typing import Type
 
 import pytest
 
+from src.core.hydrodynamics.delft3d import Delft3D
 from src.core.hydrodynamics.hydrodynamics import (
     BaseHydro,
     Hydrodynamics,
     Reef0D,
     Reef1D,
 )
-from src.core.hydrodynamics.delft3d import Delft3D
-from src.core.hydrodynamics.hydrodynamics import Hydrodynamics, Reef0D, Reef1D
 from src.core.hydrodynamics.transect import Transect
 
 
