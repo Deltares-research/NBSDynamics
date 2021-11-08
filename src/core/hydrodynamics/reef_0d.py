1b62c93f9b3c1992aee90876dfc96a5a23154ef1
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-11-08 19:54:06 +0100

    refactor(src/core/hydrodynamics/reef_0d.py): Extracted logic into its own class

---
+++ src/core/hydrodynamics/reef_0d.py
@@ -0,0 +1,64 @@
+from src.core.coral_model import Coral
+import numpy as np
+
+
+class Reef0D:
+    """Implements the 'HydrodynamicProtocol'."""
+
+    @property
+    def settings(self):
+        """Print settings of Reef0D-model."""
+        msg = f"Not yet implemented."
+        return msg
+
+    @property
+    def water_depth(self):
+        return None
+
+    @property
+    def x_coordinates(self):
+        return None
+
+    @property
+    def y_coordinates(self):
+        return None
+
+    @property
+    def xy_coordinates(self):
+        if not self.x_coordinates or self.y_coordinates:
+            raise ValueError(
+                "XY Coordinates require both 'x_coorinates' and 'y_coordinates' to be defined."
+            )
+        return np.array(
+            [
+                [self.x_coordinates[i], self.y_coordinates[i]]
+                for i in range(len(self.x_coordinates))
+            ]
+        )
+
+    @property
+    def space(self):
+        return len(self.xy_coordinates)
+
+    def initiate(self):
+        """Initiate hydrodynamic model."""
+        raise NotImplementedError
+
+    def update(self, coral, storm=False):
+        """Update hydrodynamic model.
+
+        :param coral: coral animal
+        :param storm: storm conditions, defaults to False
+
+        :type coral: Coral
+        :type storm: bool, optional
+        """
+        if storm:
+            # max(current_vel, wave_vel)
+            return None, None
+        # mean(current_vel, wave_vel, wave_per)
+        return None, None, None
+
+    def finalise(self):
+        """Finalise hydrodynamic model."""
+        raise NotImplementedError
---
+++ test/core/hydrodynamics/test_reef_0d.py
@@ -0,0 +1,46 @@
+from src.core.hydrodynamics.reef_0d import Reef0D
+from src.core.hydrodynamics.reef_1d import Reef1D
+from src.core.hydrodynamics.hydrodynamic_protocol import HydrodynamicProtocol
+import pytest
+
+
+class TestReef0D:
+    def test_init_reef0d(self):
+        test_reef = Reef1D()
+        assert isinstance(test_reef, HydrodynamicProtocol)
+        assert test_reef.settings == "Not yet implemented."
+        assert test_reef.x_coordinates == None
+        assert test_reef.y_coordinates == None
+        assert test_reef.water_depth == None
+        with pytest.raises(ValueError) as e_err:
+            test_reef.xy_coordinates
+        assert (
+            str(e_err.value)
+            == "XY Coordinates require both 'x_coorinates' and 'y_coordinates' to be defined."
+        )
+        with pytest.raises(ValueError) as e_err:
+            test_reef.space
+        assert (
+            str(e_err.value)
+            == "XY Coordinates require both 'x_coorinates' and 'y_coordinates' to be defined."
+        )
+
+    @pytest.mark.parametrize(
+        "storm_value, expected_value",
+        [
+            pytest.param(True, (None, None), id="Storm is True"),
+            pytest.param(False, (None, None, None), id="Storm is False"),
+        ],
+    )
+    def test_update(self, storm_value: bool, expected_value):
+        test_base = Reef0D()
+        result = test_base.update(coral=None, storm=storm_value)
+        assert result == expected_value
+
+    def test_initiate(self):
+        with pytest.raises(NotImplementedError):
+            Reef0D().initiate()
+
+    def test_finalise(self):
+        with pytest.raises(NotImplementedError):
+            Reef0D().finalise()
