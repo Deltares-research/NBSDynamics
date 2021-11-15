5b5528e96ffe28244853d7d7e8075d3778133f8b
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-11-15 10:15:29 +0100

    test(test/core/simulation/test_simulation.py): Extended test coverage for base_simulation.py

--- test/core/simulation/test_simulation.py
+++ test/core/simulation/test_simulation.py
@@ -1,11 +1,12 @@
 from pathlib import Path
-from typing import Callable
+from typing import Any, Callable, Optional, Tuple, Union
 
 import pytest
 
 from src.core.constants import Constants
 from src.core.environment import Environment
-from src.core.hydrodynamics.delft3d import DimrModel, FlowFmModel
+from src.core.hydrodynamics.delft3d import Delft3D, DimrModel, FlowFmModel
+from src.core.hydrodynamics.hydrodynamic_protocol import HydrodynamicProtocol
 from src.core.hydrodynamics.transect import Transect
 from src.core.simulation.base_simulation import _Simulation
 from src.core.simulation.coral_delft3d_simulation import (
@@ -13,6 +14,8 @@ from src.core.simulation.coral_delft3d_simulation import (
     CoralFlowFmSimulation,
 )
 from src.core.simulation.coral_transect_simulation import CoralTransectSimulation
+from test.utils import TestUtils
+from src.core.coral_model import Coral
 
 simulation_cases = [
     pytest.param(CoralDimrSimulation),
@@ -71,3 +74,83 @@ class TestSimulation:
             str(e_info.value)
             == "CoralModel simulation cannot run without data on temperature conditions."
         )
+
+    constants_file_case: Path = (
+        TestUtils.get_local_test_data_dir("transect_case") / "input" / "coral_input.txt"
+    )
+
+    @pytest.mark.parametrize(
+        "valid_value",
+        [
+            pytest.param(Constants(), id="As object"),
+            pytest.param(constants_file_case, id="As Path"),
+            pytest.param(constants_file_case.as_posix(), id="As String"),
+        ],
+    )
+    def test_validate_constants_with_valid_values(
+        self, valid_value: Union[Constants, str, Path]
+    ):
+        return_value = _Simulation.validate_constants(valid_value)
+        assert isinstance(return_value, Constants)
+
+    def test_validate_constants_with_not_valid_value(self):
+        with pytest.raises(NotImplementedError) as e_err:
+            _Simulation.validate_constants(42)
+        assert str(e_err.value) == f"Validator not available for {type(42)}"
+
+    @pytest.mark.parametrize(
+        "field_value, values",
+        [
+            pytest.param(
+                Coral(Constants(), 0.2, 0.2, 0.2, 0.2, 0.2, 1),
+                None,
+                id="As Object",
+            ),
+            pytest.param(
+                dict(constants=Constants(), dc=0.2, hc=0.2, bc=0.2, ac=0.2, tc=0.2),
+                None,
+                id="As dict with Constants object entry.",
+            ),
+            pytest.param(
+                dict(dc=0.2, hc=0.2, bc=0.2, ac=0.2, tc=0.2),
+                dict(constants=Constants()),
+                id="As dict with Constants in values dict entry.",
+            ),
+        ],
+    )
+    def test_validate_coral_with_valid_values(
+        self, field_value: Union[Coral, dict], values: Optional[dict]
+    ):
+        return_value = _Simulation.validate_coral(field_value, values)
+        assert isinstance(return_value, Coral)
+
+    def test_validate_coral_with_invalid_values(self):
+        with pytest.raises(NotImplementedError) as e_err:
+            _Simulation.validate_coral(42, None)
+        assert str(e_err.value) == f"Validator not available for {type(42)}"
+
+    def test_validate_coral_with_dict_without_constants(self):
+        with pytest.raises(ValueError) as e_err:
+            _Simulation.validate_coral(
+                dict(aKey="aValue"), dict(anotherKey="anotherValue")
+            )
+        assert (
+            str(e_err.value)
+            == "Constants should be provided to initialize a Coral Model."
+        )
+
+    @pytest.mark.parametrize(
+        "model_type",
+        [
+            pytest.param(DimrModel, id="DIMR model"),
+            pytest.param(FlowFmModel, id="FlowFM model"),
+            pytest.param(Transect, id="Transect"),
+        ],
+    )
+    def test_validate_hydrodynamics_present_given_hydromodel(
+        self, model_type: HydrodynamicProtocol
+    ):
+        model_in = model_type()
+        model_out = _Simulation.validate_hydrodynamics_present(model_in, dict())
+        assert isinstance(model_out, HydrodynamicProtocol)
+        assert model_in == model_out
