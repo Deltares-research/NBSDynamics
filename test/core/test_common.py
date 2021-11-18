5c9283c57587594d5c8a981b23cee2c202e1677d
Carsopre <Carsopre@users.noreply.github.com>
2021-11-18 13:35:26 +0000

    autoformat: isort & black

--- test/core/test_common.py
+++ test/core/test_common.py
@@ -15,6 +15,7 @@ class TestCommonConstants:
         const_0.tme = not const_0.tme
         assert const_0.tme == const_1.tme
 
+
 class TestCommonEnvironment:
     def test_commonenvironment_singleton(self):
         const_0 = CommonEnvironment()
