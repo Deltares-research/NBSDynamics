9fcffed3adb8a77588a9df962374809a35ac4e40
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2021-11-18 12:37:03 +0100

    test(test/core/bio_process/test_temperature.py): Added legacy 2x2 temperature tests

--- test/core/bio_process/test_temperature.py
+++ test/core/bio_process/test_temperature.py
@@ -1,5 +1,5 @@
 import pytest
-from test.core.bio_process.bio_utils import valid_coral
+from test.core.bio_process.bio_utils import valid_coral, coral_2x2
 
 from src.core.bio_process.temperature import Temperature
 from src.core.coral.coral_model import Coral
@@ -27,3 +27,41 @@ class TestTemperature:
         valid_coral.light = 600
         temp_test.coral_temperature(valid_coral)
         assert float(valid_coral.temp), pytest.approx(300)
+
+
+class TestTemperature2x2:
+    """
+    Legacy tests with a 2x2 DataReshape matrix.
+    """
+
+    @pytest.fixture(autouse=False)
+    def reshape_2x2(self) -> DataReshape:
+        return DataReshape((2, 2))
+
+    @pytest.fixture(autouse=False)
+    def temp_2x2(self, reshape_2x2: DataReshape) -> Temperature:
+        return Temperature(Constants(), [300, 300], reshape_2x2)
+
+    def test_initiation(self, temp_2x2: Temperature, reshape_2x2: DataReshape):
+        for i in range(reshape_2x2.space):
+            for j in range(reshape_2x2.time):
+                assert temp_2x2.T[i, j] == 300
+
+    def test_coral_temperature(self, temp_2x2: Temperature, coral_2x2: Coral):
+        coral_2x2.initiate_coral_morphology()
+        coral_2x2.delta_t = [0.001, 0.001]
+        coral_2x2.light = [600, 600]
+        temp_2x2.coral_temperature(coral_2x2)
+        for i in range(coral_2x2.RESHAPE.space):
+            for j in range(coral_2x2.RESHAPE.time):
+                assert float(coral_2x2.temp[i, j]), pytest.approx(300.00492692)
+
+    def test_no_tme(self, temp_2x2: Temperature, coral_2x2: Coral):
+        coral_2x2.constants.tme = False
+        coral_2x2.initiate_coral_morphology()
+        coral_2x2.delta_t = [0.001, 0.001]
+        coral_2x2.light = [600, 600]
+        temp_2x2.coral_temperature(coral_2x2)
+        for i in range(coral_2x2.RESHAPE.space):
+            for j in range(coral_2x2.RESHAPE.time):
+                assert float(coral_2x2.temp[i, j]), pytest.approx(300)
