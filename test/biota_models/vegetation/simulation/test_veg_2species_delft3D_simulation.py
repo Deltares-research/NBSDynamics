946d14a0663d0ab2b7a94dcff41111163e00c83b
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-04-20 12:30:57 +0200

    test(test_veg_2species_delft3D_simulation.py): fixed failing test due to local data

--- test/biota_models/vegetation/simulation/test_veg_2species_delft3D_simulation.py
+++ test/biota_models/vegetation/simulation/test_veg_2species_delft3D_simulation.py
@@ -1,27 +1,32 @@
 from pathlib import Path
-
-from src.biota_models.vegetation.simulation.veg_simulation_2species import _VegetationSimulation_2species
-from src.core.simulation.multiplebiota_simulation_protocol import MultipleBiotaSimulationProtocol
 from test.utils import TestUtils
 from typing import Union
+
 import pytest
+
 from src.biota_models.vegetation.model.veg_constants import VegetationConstants
-from src.biota_models.vegetation.simulation.veg_delft3D_simulation_2species import (
-    _VegDelft3DSimulation, VegFlowFmSimulation_2species,
-)
 from src.biota_models.vegetation.model.veg_model import Vegetation
+from src.biota_models.vegetation.simulation.veg_delft3D_simulation_2species import (
+    VegFlowFmSimulation_2species,
+    _VegDelft3DSimulation,
+)
+from src.biota_models.vegetation.simulation.veg_simulation_2species import (
+    _VegetationSimulation_2species,
+)
 from src.core.simulation.base_simulation import BaseSimulation
+from src.core.simulation.multiplebiota_simulation_protocol import (
+    MultipleBiotaSimulationProtocol,
+)
 
 
 class TestVegDelft3DSimulation:
     def test_veg_delft3D_simulation_ctor(self):
-        # test_dir = TestUtils.get_local_test_data_dir("delft3d_case")
         test_dir = TestUtils.get_local_test_data_dir("sm_testcase6")
-        dll_repo = TestUtils.get_external_repo("DimrDllDependencies")
-        kernels_dir = dll_repo / "kernels"
+        # dll_repo = TestUtils.get_external_repo("DimrDllDependencies")
+        # kernels_dir = dll_repo / "kernels"
 
         assert test_dir.is_dir()
-        assert kernels_dir.is_dir()
+        # assert kernels_dir.is_dir()
 
         test_case = test_dir / "input" / "MinFiles"
         species1 = "Salicornia"
@@ -34,8 +39,8 @@ class TestVegDelft3DSimulation:
             constants=veg_constants1,
             hydrodynamics=dict(
                 working_dir=test_dir / "d3d_work",
-                d3d_home=kernels_dir,
-                dll_path=kernels_dir / "dflowfm_with_shared" / "bin" / "dflowfm",
+                # d3d_home=kernels_dir,
+                # dll_path=kernels_dir / "dflowfm_with_shared" / "bin" / "dflowfm",
                 definition_file=test_case / "fm" / "test_case6.mdu",
             ),
             output=dict(
